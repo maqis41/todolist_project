f5d6dde1b1c7ec09ed0251e780b55657d084b081
maqis41 <144024666+maqis41@users.noreply.github.com>
2024-04-27 23:31:21 +0700

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+require('dotenv').config()
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  production: {
+    client: 'pg',
+    connection: `postgres://${process.env.DB_USERNAME_PROD}:${process.env.DB_PASSWORD_PROD}@${process.env.DB_HOST_PROD}/${process.env.DB_NAME_PROD}?ssl=true`,
+    migrations: {
+      directory: './src/databases/migrations'
+    },
+    seeds: {
+      directory: './src/databases/seeds'
+    }
+  },
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST_DEV,
+      database: process.env.DB_NAME_DEV,
+      user:     process.env.DB_USERNAME_DEV,
+      password: process.env.DB_PASSWORD_DEV
+    },
+    migrations: {
+      directory: './src/databases/migrations'
+    },
+    seeds: {
+      directory: './src/databases/seeds'
+    }
+  },
+};
